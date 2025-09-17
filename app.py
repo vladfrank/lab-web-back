8caf31a5a18ec07ca20b9fa52e03028d990b06e3
Франк Влад <f333vv@gmail.com>
2025-09-17 18:03:20 +0700

    Вывод цветка

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, request, redirect
+from flask import Flask, url_for, request, redirect, abort
 from werkzeug.exceptions import BadRequest, Unauthorized, Forbidden, MethodNotAllowed
 import datetime
 app = Flask(__name__)
@@ -402,3 +402,11 @@ def a():
 def a2():
         return 'со слэшем'
 
+flower_list = ('роза','тюльпан','незабудка','ромашка')
+
+@app.route('/lab2/flowers/<int:flower_id>')
+def flowers(flower_id):
+    if flower_id >= len(flower_list):
+        abort(404)
+    else:
+        return "цветок: " + flower_list[flower_id]
