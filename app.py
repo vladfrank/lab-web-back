d239480030ef9b4d5c85cf3a7e42d30a475926b6
Франк Влад <f333vv@gmail.com>
2025-09-30 14:52:17 +0700

    Имя студента

--- app.py
+++ app.py
@@ -428,4 +428,5 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Франк Владислав'
+    return render_template('example.html', name=name)
