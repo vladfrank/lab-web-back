887cc8c334ebc5fca9ded85fd58e9372747dad27
Франк Влад <f333vv@gmail.com>
2025-10-28 15:13:10 +0700

    Список пользователей

--- lab4.py
+++ lab4.py
@@ -130,6 +130,15 @@ def tree():
     return redirect('/lab4/tree')
 
 
+users = [
+    {'login': 'vlad', 'password': '123'},
+    {'login': 'bob', 'password': '555'},
+    {'login': 'vika', 'password': 'lox'},
+    {'login': 'sanya', 'password': 'moshkovo'},
+    {'login': 'egor_ivanovich', 'password': 'nstu'},
+]
+
+
 @lab4.route('/lab4/login', methods = ['GET', 'POST'])
 def login():
     if request.method == 'GET':
@@ -138,8 +147,9 @@ def login():
     login = request.form.get('login')
     password = request.form.get('password')
     
-    if login == 'vlad' and password == '123':
-        return render_template('/lab4/login.html', login=login, authorized=True)
+    for user in users:
+        if login == user['login'] and password == user['password']:
+            return render_template('/lab4/login.html', login=login, authorized=True)
     
     error = 'Неверный логин и/или пароль'
     return render_template('lab4/login.html', error=error, authorized=False)
