8d4cadd8619320cdcc2021ee9ed78fe1afa34799
Франк Влад <f333vv@gmail.com>
2025-10-28 13:35:35 +0700

    Арифметика

--- lab4.py
+++ lab4.py
@@ -1,13 +1,16 @@
 from flask import Blueprint, render_template, request, make_response,redirect
 lab4 = Blueprint('lab4', __name__)
 
+
 @lab4.route('/lab4/')
 def lab():
     return render_template('/lab4/lab4.html')
 
+
 @lab4.route('/lab4/div-form')
 def div_form():
     return render_template('/lab4/div-form.html')
+
 
 @lab4.route('/lab4/div', methods = ['POST'])
 def div():
@@ -21,3 +24,84 @@ def div():
     x2 = int(x2)
     result = x1 / x2
     return render_template('lab4/div.html', x1=x1, x2=x2, result=result)    
+
+
+@lab4.route('/lab4/sum-form')
+def sum_form():
+    return render_template('/lab4/sum-form.html')
+
+
+@lab4.route('/lab4/sum', methods = ['POST'])
+def sum():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    
+    # Если поля пустые берем значение 0
+    x1 = 0 if x1 == '' else int(x1)
+    x2 = 0 if x2 == '' else int(x2)
+    
+    result = x1 + x2
+    return render_template('lab4/sum.html', x1=x1, x2=x2, result=result)
+
+
+@lab4.route('/lab4/mul-form')
+def mul_form():
+    return render_template('/lab4/mul-form.html')
+
+
+@lab4.route('/lab4/mul', methods = ['POST'])
+def mul():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    
+    # Если поля пустые берем значение 1
+    x1 = 1 if x1 == '' else int(x1)
+    x2 = 1 if x2 == '' else int(x2)
+    
+    result = x1 * x2
+    return render_template('lab4/mul.html', x1=x1, x2=x2, result=result)
+
+
+@lab4.route('/lab4/sub-form')
+def sub_form():
+    return render_template('/lab4/sub-form.html')
+
+
+@lab4.route('/lab4/sub', methods = ['POST'])
+def sub():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    
+    # Проверка на пустые поля
+    if x1 == '' or x2 == '':
+        return render_template('lab4/sub.html', error='Оба поля должны быть заполнены!')
+    
+    x1 = int(x1)
+    x2 = int(x2)
+    result = x1 - x2
+    return render_template('lab4/sub.html', x1=x1, x2=x2, result=result)
+
+
+@lab4.route('/lab4/pow-form')
+def pow_form():
+    return render_template('/lab4/pow-form.html')
+
+
+@lab4.route('/lab4/pow', methods = ['POST'])
+def pow():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    
+    # Проверка на пустые поля
+    if x1 == '' or x2 == '':
+        return render_template('lab4/pow.html', error='Оба поля должны быть заполнены!')
+    
+    x1 = int(x1)
+    x2 = int(x2)
+    
+    # Проверка на два нуля
+    if x1 == 0 and x2 == 0:
+        return render_template('lab4/pow.html', error='Оба числа не могут быть равны нулю!')
+    
+    result = x1 ** x2
+    return render_template('lab4/pow.html', x1=x1, x2=x2, result=result)
