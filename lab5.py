43d481a6570f011234e00ad54b93f16afd6da5cd
Франк Влад <f333vv@gmail.com>
2025-11-11 15:23:52 +0700

    Вывод статей

--- lab5.py
+++ lab5.py
@@ -84,7 +84,7 @@ def login():
 def create():
     login=session.get('login')
     if not login:
-        return redirect('/lab/login')
+        return redirect('/lab5/login')
     
     if request.method == 'GET':
         return render_template('lab5/create_article.html')
@@ -103,3 +103,20 @@ def create():
     db_close(conn, cur)
     return redirect('/lab5')
 
+
+@lab5.route('/lab5/list')
+def list():
+    login = session.get('login')
+    if not login:
+        return redirect('/lab5/login')
+
+    conn, cur = db_connect()
+
+    cur.execute(f"SELECT id FROM users WHERE login='{login}';")
+    login_id = cur.fetchone()["id"]
+    
+    cur.execute(f"SELECT * FROM articles WHERE user_id='{login_id}';")
+    articles = cur.fetchall()
+
+    db_close(conn, cur)
+    return render_template('/lab5/articles.html', articles=articles)
