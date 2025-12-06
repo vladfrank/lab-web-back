f8be5af1fbb52e7de255148a6f475e48d41fac06
Франк Влад <f333vv@gmail.com>
2025-12-06 14:12:01 +0700

    Удаление напитка

--- static/lab7/main.js
+++ static/lab7/main.js
@@ -12,17 +12,22 @@ function fillBeerList() {
             let tdTitle = document.createElement('td');
             let tdTitleRus = document.createElement('td');
             let tdStrength = document.createElement('td');
+            let tdDescription = document.createElement('td'); 
             let tdActions = document.createElement('td');
 
             tdTitle.innerText = beers[i].title == beers[i].title_ru ? '' : beers[i].title;
             tdTitleRus.innerText = beers[i].title_ru;
             tdStrength.innerText = beers[i].strength;
+            tdDescription.innerText = beers[i].description;
 
             let editButton = document.createElement('button');
             editButton.innerText = 'редактировать';
 
             let delButton = document.createElement('button');
             delButton.innerText = 'удалить';
+            delButton.onclick = function() {
+                deleteBeer(i, beers[i].title_ru);
+            };
 
             tdActions.append(editButton);
             tdActions.append(delButton);
@@ -30,8 +35,20 @@ function fillBeerList() {
             tr.append(tdTitle);
             tr.append(tdTitleRus);
             tr.append(tdStrength);
+            tr.appendChild(tdDescription);
+            tr.appendChild(tdActions);
 
             tbody.append(tr);
         }
     })
+}
+
+function deleteBeer(id, title) {
+    if(! confirm(`Вы точно хотите удалить пиво "${title}"?`))
+        return;
+
+    fetch(`/lab7/rest-api/beers/${id}`, {method: 'DELETE'})
+        .then(function () {
+            fillBeerList();
+        })
 }
