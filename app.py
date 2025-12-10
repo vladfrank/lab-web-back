c18015e5a5cadd01809b6720249bf1023783e1f1
Франк Влад <f333vv@gmail.com>
2025-12-10 18:16:14 +0700

    Модель базы данных

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, url_for, request
 import datetime
+
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
@@ -11,9 +12,28 @@ from messenger import messenger
 from lab8 import lab8
 app = Flask(__name__)
 import os
+from os import path
+from flask_sqlalchemy import SQLAlchemy
+from db import db
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно-секретный секрет')
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
+
+if app.config['DB_TYPE'] == 'postgres':
+    db_name = 'vlad_frank_orm'
+    db_user = 'vlad_frank_orm'
+    db_password = '123'
+    host_ip = '127.0.0.1'
+    host_port = 5432
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+        f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+else:
+    dir_path = path.dirname(path.realpath(__file__))
+    db_path = path.join(dir_path, 'vlad_frank_orm.db')
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+
+db.init_app(app)
 
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
